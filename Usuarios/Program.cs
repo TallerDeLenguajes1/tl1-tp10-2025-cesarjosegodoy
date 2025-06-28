96c88c93b2bc40e866f7bfa85cd7123f627724ac
Cesar Jose Godoy <48810223+cesarjosegodoy@users.noreply.github.com>
2025-06-28 01:34:30 -0300

    fin ejerc02

---
+++ Usuarios/Program.cs
@@ -0,0 +1,26 @@
+﻿using classUsuario;
+using System;
+using System.Text.Json;
+
+
+
+HttpClient client = new HttpClient();
+HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/users");
+response.EnsureSuccessStatusCode();
+string? responseBody = await response.Content.ReadAsStringAsync();
+List<Usuario>? listUsuario = JsonSerializer.Deserialize<List<Usuario>>(responseBody);
+
+for (int i = 0; i < Math.Min(5, listUsuario.Count); i++)
+{
+    var usuario = listUsuario[i];
+    Console.WriteLine($"Nombre: {usuario.Name}");
+    Console.WriteLine($"Email: {usuario.Email}");
+    Console.WriteLine($"Domicilio: {usuario.Address.Street}, {usuario.Address.Suite}, {usuario.Address.City}");
+    Console.WriteLine("--------------------------------------------------");
+}
+
+
+
+string jsonString = JsonSerializer.Serialize<List<Usuario>>(listUsuario);
+
+File.WriteAllText("Usuarios.json", jsonString);
